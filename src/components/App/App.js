51f290d40ed09de1775e036757037073819dc1f8
andrektozzi <andrektozzi@gmail.com>
2022-05-18 10:57:55 -0300

    layout tela sucesso

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import Header from "../Header/Header";
 import SelecionarFilme from "../SelecionarFilme/SelecionarFilme";
 import SelecionarHorario from "../SelecionarHorario/SelecionarHorario";
 import SelecionarSecao from "../SelecionarSecao/SelecionarSecao";
+import TelaSucesso from "../TelaSucesso/TelaSucesso";
 
 export default function App () {
     return (
@@ -15,6 +16,7 @@ export default function App () {
           <Route path="/" element={<SelecionarFilme />} />
           <Route path="/filme/id" element={<SelecionarHorario />} />
           <Route path="/sessao/id" element={<SelecionarSecao />} />
+          <Route path="/sucesso" element={<TelaSucesso />} />
         </Routes>
     </BrowserRouter>
     )    
